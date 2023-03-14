581e93e27ed44bb76b882bb493bdca0540bc4828
NiallMcGurk <niallmcgurk530@gmail.com>
2023-03-14 20:42:31 +0000

    CSV and data export
    Creates a CSV which exports URL, Title, H1, Meta Description and Meta Keywords Automatically.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,36 @@
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.Connection;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import com.opencsv.CSVWriter;
+import org.jsoup.select.Elements;
 
 public class Main {
+
+    private static List<String[]> csvData;
 
     public static void main(String[] args)
     {
         String url = "https://weirandsons.ie";
+        CreateCSV();
         crawl(1, url, new ArrayList<String>());
+    }
+
+    public static void CreateCSV()
+    {
+        csvData = Header();
+        try (CSVWriter writer = new CSVWriter(new FileWriter("C:\\Users\\niall\\Documents\\GitHub\\SEOAI\\Export\\export.csv")))
+        {
+            writer.writeAll(csvData);
+        }
+        catch(IOException e){
+            System.out.println("Error creating Headers");
+        }
     }
 
     private static void crawl(int level, String url, ArrayList<String> visited)
@@ -40,13 +60,41 @@ public class Main {
                 Connection con = Jsoup.connect(url);
                 Document doc = con.get();
 
-            if(con.response().statusCode() == 200)
-            {
+            if(con.response().statusCode() == 200) {
+                String urlPaste = url;
+                String Title = doc.title();
+                Elements h1 = doc.select("h1");
+                String h1Paste = h1.text();
+                Elements metaTags = doc.getElementsByTag("meta");
+                String metaDescPaste = null;
+                String metaKeyPaste = null;
 
-                System.out.println("Link: " + url);
-                System.out.println(doc.title());
-                System.out.println(doc.select("h1"));
-                System.out.println(doc.select("meta"));
+                for (Element metaTag : metaTags) {
+                    String name = metaTag.attr("name");
+                    String content = metaTag.attr("content");
+
+
+                    if (name.equals("description") ) {
+                        metaDescPaste = content;
+                    }
+                    if (name.equals("keywords") ) {
+                        metaKeyPaste = content;
+                    }
+
+                }
+                String[] line = {urlPaste, Title, h1Paste, metaDescPaste, metaKeyPaste};
+
+                csvData.add(line);
+                try (CSVWriter writer = new CSVWriter(new FileWriter("C:\\Users\\niall\\Documents\\GitHub\\SEOAI\\Export\\export.csv"))) {
+                    writer.writeAll(csvData);
+                } catch (IOException e) {
+                    System.out.println("Error creating new Line");
+                }
+
+                System.out.println("Line:" + url + " | " + Title);
+                System.out.println(metaTags);
+                System.out.println("********************" + metaDescPaste);
+                System.out.println("********************" + metaKeyPaste);
                 v.add(url);
                 return doc;
             }
@@ -56,5 +104,13 @@ public class Main {
             System.out.println("Error");
             return null;
         }
+    }
+
+    private static List<String[]> Header()
+    {
+        String[] header = {"URL", "Title", "H1", "Meta Description", "Meta Keywords"};
+        List<String[]> list = new ArrayList<>();
+        list.add(header);
+        return list;
     }
 }
