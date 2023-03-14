249c9af8b4da3bdaed11c7ae27da26a843cad977
NiallMcGurk <niallmcgurk530@gmail.com>
2023-03-14 19:39:46 +0000

    Web Crawler function
    Initialised Web Crawler Function, working correctly but cannot handle some errors (URL's vs URI's).

--- src/Main.java
+++ src/Main.java
@@ -23,9 +23,8 @@ public class Main {
             {
                 for(Element link : doc.select("a[href]"))
                 {
-
                     String next_link = link.absUrl("href");
-                    if (visited.contains(next_link) == false && !visited.contains("tel:"))
+                    if (visited.contains(next_link) == false && !next_link.contains("tel:") && !next_link.contains("#contentarea"))
                     {
                         crawl(level++, next_link, visited);
                     }
@@ -38,14 +37,16 @@ public class Main {
     private static Document request(String url, ArrayList<String> v)
     {
         try {
-            Connection con = Jsoup.connect(url);
-            Document doc = con.get();
+                Connection con = Jsoup.connect(url);
+                Document doc = con.get();
 
             if(con.response().statusCode() == 200)
             {
 
                 System.out.println("Link: " + url);
                 System.out.println(doc.title());
+                System.out.println(doc.select("h1"));
+                System.out.println(doc.select("meta"));
                 v.add(url);
                 return doc;
             }
